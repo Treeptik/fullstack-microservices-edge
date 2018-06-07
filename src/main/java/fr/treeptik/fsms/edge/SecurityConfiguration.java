0ffd1139b7ed095b79d0eec85d4e8abb566eb48e
William Bartlett <w.bartlett@treeptik.fr>
2018-06-07 16:25:28 +0200

    initial commit

---
+++ src/main/java/fr/treeptik/fsms/edge/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package fr.treeptik.fsms.edge;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableOAuth2Sso
+//@EnableResourceServer
+//@Order(Ordered.HIGHEST_PRECEDENCE)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.anyRequest().permitAll()
+			//.antMatchers("/auth/**").permitAll()
+			//.anyRequest().authenticated()
+			.and().csrf().disable()
+			.formLogin();
+	}
+}
